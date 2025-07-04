7376d006bb790c4c9f92205a4f470066d83927e3
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-05 05:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const makeChange = (c) => {
     let q = 0, d = 0, n = 0, p = 0;
   // your name here
 	if(c>=25){
-		let q=Math.floor(c/25);
+	q=Math.floor(c/25);
 	c%=25;
 	}
 	if(c>=10){
-			let d=Math.floor(c/10);
+	d=Math.floor(c/10);
 	c%=10;
 	}
 	if(c>=5){
-		let n=Math.floor(c/5);
+		n=Math.floor(c/5);
 		c%=5;
 	}
-	let p=c;
+	 p=c;
 	return {
 		"q":q,
 		"d":d,
